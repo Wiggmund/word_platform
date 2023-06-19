ae1156247303e8bafb581d0ffbf8ec6bc7fb4c96
Wiggmund <mirracleourworld@gmail.com>
2023-06-19 16:36:06 +0300

    Add public available endpoints

--- src/main/java/com/example/word_platform/WordPlatformApplication.java
+++ src/main/java/com/example/word_platform/WordPlatformApplication.java
@@ -1,6 +1,5 @@
 package com.example.word_platform;
 
-import com.example.word_platform.dto.user.UserCreateDto;
 import com.example.word_platform.repository.RefreshTokenRepo;
 import com.example.word_platform.repository.UserRepo;
 import com.example.word_platform.service.user.UserService;
@@ -12,21 +11,21 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class WordPlatformApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(WordPlatformApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(WordPlatformApplication.class, args);
+    }
 
-  @Bean
-  public CommandLineRunner run(
-      UserService userService,
-      UserRepo userRepo,
-      RefreshTokenRepo refreshTokenRepo
-  ) {
-    return args -> {
-      refreshTokenRepo.deleteAll();
-      refreshTokenRepo.flush();
-      userRepo.deleteAll();
-      userRepo.flush();
-    };
-  }
+    @Bean
+    public CommandLineRunner run(
+            UserService userService,
+            UserRepo userRepo,
+            RefreshTokenRepo refreshTokenRepo
+    ) {
+        return args -> {
+            refreshTokenRepo.deleteAll();
+            refreshTokenRepo.flush();
+            userRepo.deleteAll();
+            userRepo.flush();
+        };
+    }
 }
--- src/main/java/com/example/word_platform/config/SecurityConfig.java
+++ src/main/java/com/example/word_platform/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import com.example.word_platform.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -24,16 +27,22 @@ public class SecurityConfig {
     return httpSecurity
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests((auth) -> auth
-            .requestMatchers("/api/v1/auth/**")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
+            .requestMatchers("/api/v1/auth/**").permitAll()
+            .requestMatchers(HttpMethod.GET,"/api/v1/words").permitAll()
+            .requestMatchers(HttpMethod.GET,"/api/v1/wordlists").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/attributes").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/users").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/users/*/words").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/users/*/wordlists").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/v1/users/*/wordlists/*/words").permitAll()
+            .anyRequest().authenticated()
         )
         .sessionManagement((session) -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         )
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .exceptionHandling(ex -> ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
         .build();
   }
 }
